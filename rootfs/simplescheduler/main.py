3a49acd565d90d0dafc815f02f125e5b588109b1
htht2001 <97393694+htht2001@users.noreply.github.com>
2024-09-23 13:16:09 +0300

    Update main.py

--- rootfs/simplescheduler/main.py
+++ rootfs/simplescheduler/main.py
@@ -741,7 +741,7 @@ def call_ha(eid_list, action, passedvalue, friendly_name):
             if domain[0] == "climate" and value != "":
                 if value[0] == "F":
                     v = value[1:]
-                    command_url = simpleschedulerconf.HASSIO_URL + "/services/climate/set_fan_mode"
+                    command_url = simpleschedulerconf.HASSIO_URL + "/services/climate/climate.set_fan_mode"
                     postdata = '{"entity_id":"%s","fan_mode":"%s"}' % (eid, v.lower())
                     command = "Setting"
                     extra = "temperature to " + v + '°'
