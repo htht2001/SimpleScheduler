3e8ecafd7f3fa67ebf98ec65c3bdf58cec7c9016
htht2001 <97393694+htht2001@users.noreply.github.com>
2024-09-23 15:53:17 +0300

    Update main.py

--- rootfs/simplescheduler/main.py
+++ rootfs/simplescheduler/main.py
@@ -287,7 +287,7 @@ def utility_processor():
                         extra = '<span class="event-type-to"><i class="mdi mdi-thermometer" aria-hidden="true"></i>' + v + '&deg;</span>'
                     if v[0] == 'F':
                         v = v[1:]
-                        extra = '<span class="event-type-to"><i class="mdi mdi-fan" aria-hidden="true"></i>' + v
+                        extra = '<span class="event-type-to"><i class="mdi mdi-fan" aria-hidden="true"></i>' + v + '&deg;</span>'
                     if v[0] == 'M':
                         v = v[1:]
                         extra = '<span class="event-type-to"><i class="mdi mdi-power" aria-hidden="true"></i>' + v + '&deg;</span>'
