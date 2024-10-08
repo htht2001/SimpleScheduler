28a4a3e085f9880c2b8da58565bc8396afd861bb
ye0534143454 <YE0534143454@GMAIL.COM>
2024-10-08 21:59:32 +0300

    Update main.py

--- rootfs/simplescheduler/main.py
+++ rootfs/simplescheduler/main.py
@@ -288,19 +288,19 @@ def utility_processor():
                         "F2": "<i class='mdi mdi-fan-speed-2' ></i>",
                         "F3": "<i class='mdi mdi-fan-speed-3' ></i>",
 
-                        "S0": '<span class="event-type-p"><i class="mdi mdi-arrow-oscillating-Off" aria-hidden="true"></i>%</span>',
-                        "S1": '<span class="event-type-p"><i class="mdi mdi-arrow-updown" aria-hidden="true"></i>%</span>',
-                        "S2": '<span class="event-type-p"><i class="mdi mdi-arrow-Leftright" aria-hidden="true"></i>%</span>',
-                        "S3": '<span class="event-type-p"><i class="mdi mdi-arrow-all" aria-hidden="true"></i>%</span>',
+                        "S0": '<span class="event-type-p"><i class="mdi mdi-arrow-oscillating-Off" aria-hidden="true"></i></span>',
+                        "S1": '<span class="event-type-p"><i class="mdi mdi-arrow-updown" aria-hidden="true"></i></span>',
+                        "S2": '<span class="event-type-p"><i class="mdi mdi-arrow-Leftright" aria-hidden="true"></i></span>',
+                        "S3": '<span class="event-type-p"><i class="mdi mdi-arrow-all" aria-hidden="true"></i></span>',
 
-                        "MO": '<span class="event-type-p"><i class="mdi mdi-power" aria-hidden="true"></i>%</span>',
-                        "MC": '<span class="event-type-p"><i class="mdi mdi-Snow-flake" aria-hidden="true"></i>%</span>',
-                        "MH": '<span class="event-type-p"><i class="mdi mdi-fire" aria-hidden="true"></i>%</span>',
+                        "MO": '<span class="event-type-p"><i class="mdi mdi-power" aria-hidden="true"></i></span>',
+                        "MC": '<span class="event-type-p"><i class="mdi mdi-Snow-flake" aria-hidden="true"></i></span>',
+                        "MH": '<span class="event-type-p"><i class="mdi mdi-fire" aria-hidden="true"></i></span>',
                         "MD": "dry",
-                        "MA": '<span class="event-type-p"><i class="mdi mdi-thermostat-auto" aria-hidden="true"></i>%</span>',
-                        "MR": '<span class="event-type-p"><i class="mdi mdi-fan" aria-hidden="true"></i>%</span>',
+                        "MA": '<span class="event-type-p"><i class="mdi mdi-thermostat-auto" aria-hidden="true"></i></span>',
+                        "MR": '<span class="event-type-p"><i class="mdi mdi-fan" aria-hidden="true"></i></span>',
 
-                        "T": '<span class="event-type-p"><i class="mdi mdi-thermometer" aria-hidden="true"></i>%</span>',
+                        "T": '<span class="event-type-p"><i class="mdi mdi-thermometer"aria-hidden="true"></i></span>',
                     }
                     v = v.replace("_", " ")
                     if "F" in v:
