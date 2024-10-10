066d6b34e06bcf98440581b89184950de1c706b1
ye0534143454 <YE0534143454@GMAIL.COM>
2024-10-10 03:42:23 +0300

    Update main.py

--- rootfs/simplescheduler/main.py
+++ rootfs/simplescheduler/main.py
@@ -790,9 +790,9 @@ def call_ha(eid_list, action, passedvalue, friendly_name):
                     d = re.search(r'T(\d+(\.\d+)?)', value)
                     tempe = d.group(1)
                     command_url = simpleschedulerconf.HASSIO_URL + "/services/climate/set_temperature"
-                    postdata = '{"entity_id":"%s","temperature":"%s"}' % (eid, tempe)
-                    command = "Setting" 
-                    extra = "temperature to " + tempe + '°'
+                    postdata  =  '{"entity_id":"%s","temperature":"%s"}' % (eid, tempe)
+                    command  =  "Setting"
+                    extra  =  "temperature to " + tempe + '°'
                     printlog("SCHED: %s [%s] %s" % (command, friendly_name.get(eid, eid), extra))
                     call_ha_api(command_url, postdata)
 
